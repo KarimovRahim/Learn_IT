b60a8b64cad29fba871928a226cf3ee28d1a63cd
Abdur <karimovaleks19@gmail.com>
2026-02-17 17:09:50 +0500

    update_AOS-animation2.6

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -161,7 +161,7 @@ const Services = () => {
               </Button>
             </div>
 
-            <div className="grid grid-cols-1 gap-4">
+            <div className="grid grid-cols-2 gap-4">
               {industries.map((industry, index) => (
                 <div
                   key={index}
