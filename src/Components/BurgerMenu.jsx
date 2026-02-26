ff42f821649d225651c9426ec47af568ed18f190
Abdur <karimovaleks19@gmail.com>
2026-02-26 12:53:07 +0500

    update the drawer 2.4

--- src/Components/BurgerMenu.jsx
+++ src/Components/BurgerMenu.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion'
 import { createPortal } from 'react-dom';
 import { Link, useLocation } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
